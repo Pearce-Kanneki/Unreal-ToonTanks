08bb5930ee85a6f9d1b5c43a06b06cc7efe71014
Kanneki Chen <pearce712134@gmail.com>
2021-12-13 20:50:39 +0800

    feat init

---
+++ Source/ToonTanks/Projectile.cpp
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Projectile.h"
+
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/DamageType.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "GameFramework/PlayerController.h"
+
+// Sets default values
+AProjectile::AProjectile() {
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = false;
+
+    ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
+    RootComponent = ProjectileMesh;
+
+    ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement Component"));
+    ProjectileMovementComponent->MaxSpeed = 1300.f;
+    ProjectileMovementComponent->InitialSpeed = 1300.f;
+
+    TrailParticles = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("Smoke Trail"));
+    TrailParticles->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void AProjectile::BeginPlay() {
+    Super::BeginPlay();
+    ProjectileMesh->OnComponentHit.AddDynamic(this, &AProjectile::OnHit);
+    if (LaunchSound) {
+        UGameplayStatics::PlaySoundAtLocation(this, LaunchSound, GetActorLocation());
+    }
+}
+
+// Called every frame
+void AProjectile::Tick(float DeltaTime) {
+    Super::Tick(DeltaTime);
+}
+
+void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) {
+    // UE_LOG(LogTemp, Warning, TEXT("OnHit"));
+    // UE_LOG(LogTemp, Warning, TEXT("HitComp: %s"), *HitComp->GetName());
+    // UE_LOG(LogTemp, Warning, TEXT("OtherActor: %s"), *OtherActor->GetName());
+    // UE_LOG(LogTemp, Warning, TEXT("OtherComp: %s"), *OtherComp->GetName());
+    AActor* MyOwner = GetOwner();
+    if (MyOwner == nullptr){
+        Destroy();
+        return;
+    }
+
+    AController* MyOwnerInstigator = MyOwner->GetInstigatorController();
+    UClass* DamageTypeClass = UDamageType::StaticClass();
+
+    if (OtherActor && OtherActor != this && OtherActor != MyOwner) {
+		UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwnerInstigator, this, DamageTypeClass);
+        if(HitParticles){
+            UGameplayStatics::SpawnEmitterAtLocation(this, HitParticles, GetActorLocation(), GetActorRotation());
+        }
+        if (HitSound) {
+            UGameplayStatics::PlaySoundAtLocation(this, HitSound, GetActorLocation());
+        }
+        if (HitCameraShakeClass) {
+            // UE 4.25 - ClientPlayUCameraShake; UE 4.26+ ClientStartCameraShake
+            GetWorld()->GetFirstPlayerController()->ClientStartCameraShake(HitCameraShakeClass);
+        }
+        
+    }
+    Destroy();
+}
